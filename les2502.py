3f2738aa7ee1672fd193d61d8c8012b518e5b80f
SkoryninaKaterina <skorynina.katerina95@gmail.com>
2025-02-27 11:56:56 +0300

    зроблено дз 2502

--- les2502.py
+++ les2502.py
@@ -1,82 +1,68 @@
 # Завдання 1
-# Створіть клас Recipe з атрибутами
-#  name – назва страви
-#  ingredients – список продуктів
-#  text – текст рецепту
-#  time – час приготування
-# методи:
-#  __str__(self) – повертає назву страви
-#  __contains__(self, item) – перевіряє чи є інгредієнт в
-# рецепті
-#  __gt__(self, other) – перевіряє чи є час приготування self
-# більшим за other
-#  display_info(self) – виводить всю інформацію про рецепт
-# Створіть декілька рецептів та добавте їх у список.
-# Виведіть назви тих рецептів, які містять інгредієнт томат
-# Виведіть повну інформацію рецепта з найменшим часом
-# приготування, скористайтесь функцією min
-# Приклад рецептів:
-# Recipe("Піца",
+# Створіть клас Pet з атрибутами
+#  name – ім’я тварини
+#  satiety – рівень ситості(від 0 до 100, за замовчуванням 50)
+#  energy – рівень енергії (від 0 до 100, за замовчуванням 50)
+# Методи:
+#  sleep() – збільшує energy до 100
+#  eat(food_amont) – їсть, збільшує satiety на food_amount
+#  play(activity_level) – абстрактний метод
+#  make_sound() – просто pass
+# Створіть клас Cat
+# Методи:
+#  play(activity_level) – якщо satiety > 60, зменшує energy на
+# 2*acticity_level та satiety на acticity_level
+#  make_sound() – виводить ‘Мяу’
+#  catch_mouse() – якщо energy > 30, ловить мишу. Якщо
+# satiety > 40, то грається з мишею, інакше їсть
+# Створіть клас Dog
+# Методи:
+#  play(activity_level) – якщо satiety > 15, зменшує energy на
 # Домашнє завдання
-#  ["борошно", "вода", "дріжджі", "томат", "сир"],
-#  "Готуємо тісто, додаємо інгредієнти та запікаємо",
-#  30)
-#
-#  Recipe("Салат",
-#  ["томат", "огірок", "зелень", "олія"],
-#  "Нарізаємо овочі, додаємо зелень та поливаємо
-# олією",
-#  10)
-#
-#  Recipe("Суп",
-#  ["вода", "картопля", "морква", "м'ясо"],
-#  "Варимо всі інгредієнти до готовності",
-#  45)
+# acticity_level//2 та satiety на acticity_level//2
+#  make_sound() – виводить ‘Гав’
+#  fetch_ball() – ловить м’яча якщо satiety>10, зменшує
+# energy на 5
 
-
-class Recipe:
-    def __init__(self, name, ingredients, text, time):
+class Pet:
+    def __init__(self, name, satiety=50, energy=50):
         self.name = name
-        self.ingredients = ingredients
-        self.text = text
-        self.time = time
+        self.satiety = satiety
+        self.energy = energy
+
+    def sleep(self):
+        """Сон відновлює енергію до 100."""
+        self.energy = 100
+
+    def eat(self, food_amount):
+        """Їжа збільшує ситість, але не більше 100."""
+        self.satiety = min(self.satiety + food_amount, 100)
+
+    def play(self, activity_level):
+        pass
+
+    def make_sound(self):
+        pass
 
     def __str__(self):
-        return self.name
-
-    def __contains__(self, item):
-        return item in self.ingredients
-
-    def __gt__(self, other):
-        return self.time > other.time
-
-    def __lt__(self, other):
-        return self.time < other.time
-
-    def __repr__(self):
-        return f"Recipe({self.name}, {self.ingredients}, {self.text}, {self.time})"
-
-    def display_info(self):
-        print(f"🔹 Назва: {self.name}\n🛒 Інгредієнти: {', '.join(self.ingredients)}\n📜 Рецепт: {self.text}\n⏳ Час: {self.time} хвилин\n")
+        return f"{self.name} | Ситість: {self.satiety}, Енергія: {self.energy}"
 
 
-# Створюємо рецепти
-recipes = [
-    Recipe("Піца", ["борошно", "вода", "дріжджі", "томат", "сир"], "Готуємо тісто, додаємо інгредієнти та запікаємо", 30),
-    Recipe("Салат", ["томат", "огірок", "зелень", "олія"], "Нарізаємо овочі, додаємо зелень та поливаємо олією", 10),
-    Recipe("Суп", ["вода", "картопля", "морква", "м'ясо"], "Варимо всі інгредієнти до готовності", 45)
-]
+class Cat(Pet):
+    def play(self, activity_level):
+        if self.satiety > 60:
+            self.energy = max(self.energy - 2 * activity_level, 0)
+            self.satiety = max(self.satiety - activity_level, 0)
 
-# Вивести рецепти, що містять "томат"
-print("🍅 Рецепти, які містять томат:")
-for recipe in recipes:
-    if "томат" in recipe:
-        print(f"- {recipe}")
+    def make_sound(self):
+        print('Мяу')
 
-# Знайти рецепт із найменшим часом приготування
-fastest_recipe = min(recipes)
-print("\n⏳ Рецепт із найменшим часом приготування:")
-fastest_recipe.display_info()
+    def catch_mouse(self):
+        if self.energy > 30:
+            if self.satiety > 40:
+                print(f"{self.name} грається з мишею.")
+
+
 
 
 
